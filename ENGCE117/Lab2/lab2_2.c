2fa0d740b9cf804cbc69c2c1ce7d785bbef2d84a
Pocky <mickycoma@gmail.com>
2025-12-13 16:50:37 +0700

    Update lab2_2.c

--- ENGCE117/Lab2/lab2_2.c
+++ ENGCE117/Lab2/lab2_2.c
@@ -7,33 +7,27 @@ char* reverse(char str1[]);
 
 int main() {
     char text[50] = "I Love You";
-    char *out;               
-    
+    char *out;
+
     out = reverse(text);
-
-    free(out); //คืนหน่วยความจำ
-
     // ใช้ printf เพื่อตรวจสอบผลลัพธ์
-    printf("Original: %s\n", text);
-    printf("Reversed: %s\n", out);
+    printf("reverse: %s\n", out);
     
     return 0;
 }
 
 char* reverse(char str1[]) {
-    int i = 0;
-    int n = 0;
-    int len = strlen(str1); //เก็บความยาวของข้อความที่่รับมา
+    int i,j = 0;
+    int length = strlen(str1); //เก็บความยาวของข้อความที่่รับมา 
+    char *str2 = (char *)malloc(length + 1 ); 
 
-    char *str2 = (char *)malloc(len + 1 * sizeof(char)); 
+    //Loopในการย้ายข้อความ
+    for (i = length -1; i >= 0; i--) { 
+        str2[j] = str1[i];
+        j++;
 
-    //Loop ย้ายข้อความ input ไป str2
-    for (i = len -1; i >= 0; i--) { 
-        str2[n] = str1[i];
-        n++;
     }
-    str2[n] = '\0';
-
-    //ส่งค่าstr2ไปยังmain
+    
+    str2[j] = '\0';
     return str2;
 }
