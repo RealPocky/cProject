5e124e96b1f3ee7a875fa563de66ea88fa97edd5
Pocky <mickycoma@gmail.com>
2025-12-21 22:56:27 +0700

    Add files via upload

---
+++ ENGCE117/Lab4/Lab4_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void go (int **p, int *z){
+    *p = z; // ทำให้ p ชี้ไปที่ z
+
+}
+    
+int main() {
+    int *a;
+    int b = 10;
+    int c = 20;
+
+    go(&a, &b);
+    printf("b = %d &b = %p\n", b, &b);
+    printf("*a = %d a = %p\n", *a, a);
+
+    go(&a, &c);
+    printf("c = %d &c = %p\n", c, &c);
+    printf("*a = %d a = %p\n", *a, a);
+    return 0;
+
+}
